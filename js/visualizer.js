ade06ba6fe7a02155c19a1a85c2f93baa4279c7d
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2020-06-20 15:10:12 +0530

    Fix. Dynamic Node Drawing | Tuhin
    Signed-off-by: Tuhin Karmakar <tuhinkarmakar3882@gmail.com>

--- js/visualizer.js
+++ js/visualizer.js
@@ -1,3 +1,5 @@
+let peculiarData;
+
 function createNodes() {
     let nodeInput = document.getElementById('nodeInput');
 
@@ -11,18 +13,52 @@ function createNodes() {
                 for (let data in nodeData) {
                     let xPos = nodeData[data].xPos;
                     let yPos = nodeData[data].yPos;
+                    let textData = nodeData[data].text;
+                    fill(color(245, 6, 128));
                     circle(xPos, yPos, radius);
+                    fill(color(0, 0, 0));
+                    textSize(16);
+                    textStyle(BOLD);
+                    text(textData, xPos, yPos);
                 }
             }
 
             draw();
+            peculiarData = nodeData;
         })
         .fail(response => {
             console.log(response.responseJSON);
         });
 }
 
+let x, y;
+
 function setup() {
-    createCanvas(1000, 500);
+    createCanvas(1350, 600);
+    x = width / 2;
+    y = height;
 }
 
+function draw() {
+    background(220);
+    radius = 30;
+    for (let data in peculiarData) {
+        let xPos = peculiarData[data].xPos;
+        let yPos = peculiarData[data].yPos;
+        let textData = peculiarData[data].text;
+        // peculiarData[data].xPos = peculiarData[data].xPos + (random(-3, 3));
+        // Moving up at a constant speed
+        // rect(xPos,yPos, yPos,xPos);
+        // peculiarData[data].yPos = peculiarData[data].yPos - (random(-3, 3));
+        fill(color(225, 225, 0));
+        circle(xPos, yPos, radius);
+        fill(color(0, 0, 0));
+        textSize(16);
+        textStyle(BOLD);
+        text(textData, xPos, yPos);
+        if (peculiarData[data].yPos < 0) {
+            peculiarData[data].yPos = height;
+        }
+    }
+
+}
